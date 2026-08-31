11334647656eb0081f871a6063f3fff80ab352af
moses <mosesdimkpa1@gmail.com>
2026-08-31 11:00:54 +0100

    Add /admin_logs command and file logger to help debug missing messages

--- forwarder.py
+++ forwarder.py
@@ -1,5 +1,23 @@
 import os
 import asyncio
+import sys
+
+class Logger(object):
+    def __init__(self):
+        self.terminal = sys.stdout
+        self.log = open("forwarder.log", "a", encoding="utf-8")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+        self.log.flush()
+
+    def flush(self):
+        self.terminal.flush()
+        self.log.flush()
+
+sys.stdout = Logger()
+sys.stderr = sys.stdout
 import re
 from telethon import TelegramClient, events
 from telethon.tl.types import MessageMediaWebPage
